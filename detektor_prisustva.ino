cd5cd66fbef5513affa785288cf060a693baf06b
blukovac <lb130415d@student.etf.bg.ac.rs>
2017-09-26 09:10:53 +0200

    Nova verzija projekta.

--- detektor_prisustva.ino
+++ detektor_prisustva.ino
@@ -5,20 +5,15 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-//#include <ESP8266mDNS.h>
-//#include <Time.h>
-//#include <SPI.h>
 
 extern "C" {
 #include<user_interface.h>
 }
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
-#define MAX 2
+#define MAX 3
+#define date (convertDateToStr(rtc.GetDateTime()))
 
-//MDNSResponder mdns;
-
-// Replace with your network credentials
 const char* ssid = "Proba";
 
 ESP8266WebServer server(80);
@@ -26,6 +21,7 @@ ESP8266WebServer server(80);
 String webPage = "";
 
 File myFile;
+File myFile2;
 
 String prijavljeni[MAX];
 
@@ -43,27 +39,14 @@ void setup(void)
   Serial.begin(115200);
   Serial.println();
   Serial.print("Configuring access point...");
-  //IPAddress ip(192, 168, 1, 7);
-  //WiFi.hostname(prijava);
-  //wifi_station_set_hostname( "test11" );
   WiFi.softAP(ssid);
   Serial.println("");
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
-  //Serial.print("Hostname: ");
-  //Serial.println(WiFi.hostname());
-
-  //if (mdns.begin("prijava", WiFi.softAPIP())) {
-    //Serial.println("MDNS responder started");
-  //}
   
   server.on("/", [](){
     server.send(200, "text/html", webPage);
   });
-  //server.on("/Metoda1", [](){
-    //server.send(200, "text/html", webPage);
-    //delay(1000); 
-  //});
   server.begin();
   Serial.println("HTTP server started");
 
@@ -74,28 +57,15 @@ void setup(void)
   }
   else
   {
-    Serial.println("SD card initialization failed");
+    Serial.println("SD card initialization failed.");
     return;
   }
-  
-  for (int i = 0; i < MAX; prijavljeni[i++] = "0");
+
+  for (int i = 0; i < MAX; prijavljeni[i++] = "/");
   
   rtc.Begin();
   rtc.Enable32kHzPin(false);
   rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
-
-  String datum = convertDateToStr(rtc.GetDateTime());
-  myFile = SD.open(datum + ".txt", FILE_WRITE);
-  if (myFile)
-  {
-    Serial.println("Creating file ARP_" + datum + ".txt...");
-    myFile.close();
-    Serial.println("Done.");
-  }
-  else
-  {
-    Serial.println("error opening ARP_" + datum + ".txt");
-  }
 }
  
 void loop(void)
@@ -108,153 +78,92 @@ void loop(void)
 
 void client_status()
 {
-  //unsigned char number_client = wifi_softap_get_station_num();
   struct station_info *stat_info;
-  stat_info = wifi_softap_get_station_info();
 
-  //struct ip_addr *IPaddress;
-  //IPAddress address;
+  struct ip_addr *IPaddress;
+  IPAddress address;
 
-  String datum = convertDateToStr(rtc.GetDateTime());
-
-  boolean kandidat = false;
-  
-  while (stat_info != NULL)
-  {
-    kandidat = true;
-    
-    String t = convertToStr(rtc.GetDateTime());
-    String mac = "";
-    mac += ((String(stat_info->bssid[0],HEX).length()) == 2)? String(stat_info->bssid[0],HEX) : "0" + String(stat_info->bssid[0],HEX);
-    mac += ((String(stat_info->bssid[1],HEX).length()) == 2)? String(stat_info->bssid[1],HEX) : "0" + String(stat_info->bssid[1],HEX);
-    mac += ((String(stat_info->bssid[2],HEX).length()) == 2)? String(stat_info->bssid[2],HEX) : "0" + String(stat_info->bssid[2],HEX);
-    mac += ((String(stat_info->bssid[3],HEX).length()) == 2)? String(stat_info->bssid[3],HEX) : "0" + String(stat_info->bssid[3],HEX);
-    mac += ((String(stat_info->bssid[4],HEX).length()) == 2)? String(stat_info->bssid[4],HEX) : "0" + String(stat_info->bssid[4],HEX);
-    mac += ((String(stat_info->bssid[5],HEX).length()) == 2)? String(stat_info->bssid[5],HEX) : "0" + String(stat_info->bssid[5],HEX);
-
-    //IPaddress = &stat_info->ip;
-    //address = IPaddress->addr;
-    //Serial.print((address));
-
-    boolean prisutan = false;
-
-    myFile = SD.open(datum + ".txt");
-    if (myFile)
-    {
-      int br_korisnika = 1;
-      
-      while (myFile.available() && !prisutan) 
-      {     
-        String line_mac = myFile.readStringUntil('|');
-
-        String line_range;
-        int br = 3;
-        while (br--)
-        {
-          if (br > 0)
-          {
-            line_range = myFile.readStringUntil('|');
-          }
-          else
-          {
-            line_range = myFile.readStringUntil('\n');
-          }
-        }
-        
-        //String line_ip = "";
-        //int i = 0;
-        //for (; line[i] != '|'; line_ip[i] += line[i++]);
-
-        if (line_range == "0")
-        {
-          continue;
-        }
-
-        if (mac == line_mac)
-        {
-          prisutan = true;
-          
-          prijavljeni[br_korisnika-1] = "1 " + mac;
-        }
-        else
-        {
-          if (prijavljeni[br_korisnika-1][1] == ' ')
-          {
-            prijavljeni[br_korisnika-1] = line_mac + "|" + convertToStr(rtc.GetDateTime()) + "|0";
-          }
-        }
-
-        line_mac = myFile.readStringUntil('\n');
-        line_range = myFile.read();
-
-        br_korisnika++;
-      }
-      myFile.close();
-    }
-    else
-    {
-      Serial.println("error opening ARP_" + datum + ".txt");
-    }
-    
-    if (!prisutan)
-    {
-      myFile = SD.open(datum + ".txt", FILE_WRITE);
-      if (myFile)
-      {
-        Serial.println("Writing new students...");
-        //myFile.print((address));
-        //myFile.print("|");
-        myFile.print(mac);
-        myFile.print("|");
-        myFile.print(t);
-        myFile.println("|1");
-        myFile.close();
-        Serial.println("Done.");
-      }
-      else
-      {
-        Serial.println("error opening ARP_" + datum + ".txt");
-      }
-    }
-
-    stat_info = STAILQ_NEXT(stat_info, next);
-  }
-
-  if (!kandidat)
-  {
-    for (int i = 0; i < MAX; i++)
-    {
-      if (prijavljeni[i][1] == ' ')
-      {
-        String prijavljeni_mac = "";
-        for (int j = 1; prijavljeni[i][j] != '\0'; prijavljeni_mac += prijavljeni[i][j++]);
-        
-        prijavljeni[i] = prijavljeni_mac + "|" + convertToStr(rtc.GetDateTime()) + "|0";
-      }
-    }
-  }
-
-  myFile = SD.open(datum + ".txt", FILE_WRITE);
+  myFile = SD.open("reg.txt");
   if (myFile)
   {
-    for (int i = 0; i < MAX; i++)
+    int br_korisnika = 1;
+    
+    while (myFile.available()) 
     {
-      if (prijavljeni[i][1] != ' ' && prijavljeni[i] != "0")
-      {
-        Serial.println("Writing an offline student...");
-        myFile.println(prijavljeni[i]);
-        Serial.println("Done.");
+      stat_info = wifi_softap_get_station_info();
+      
+      boolean prisutan = false;
+      boolean novi = false;
 
-        prijavljeni[i] = "0";
+      String t = convertToStr(rtc.GetDateTime());     
+      String line_ip = myFile.readStringUntil('|');
+     
+      while (stat_info != NULL)
+      {
+        String mac = "";
+        mac += ((String(stat_info->bssid[0],HEX).length()) == 2)? String(stat_info->bssid[0],HEX) : "0" + String(stat_info->bssid[0],HEX);
+        mac += ((String(stat_info->bssid[1],HEX).length()) == 2)? String(stat_info->bssid[1],HEX) : "0" + String(stat_info->bssid[1],HEX);
+        mac += ((String(stat_info->bssid[2],HEX).length()) == 2)? String(stat_info->bssid[2],HEX) : "0" + String(stat_info->bssid[2],HEX);
+        mac += ((String(stat_info->bssid[3],HEX).length()) == 2)? String(stat_info->bssid[3],HEX) : "0" + String(stat_info->bssid[3],HEX);
+        mac += ((String(stat_info->bssid[4],HEX).length()) == 2)? String(stat_info->bssid[4],HEX) : "0" + String(stat_info->bssid[4],HEX);
+        mac += ((String(stat_info->bssid[5],HEX).length()) == 2)? String(stat_info->bssid[5],HEX) : "0" + String(stat_info->bssid[5],HEX);
+
+        IPaddress = &stat_info->ip;
+        address = IPaddress->addr;
+        String ip = IpAddress2String(address);
+        
+        if (ip == line_ip)
+        {
+          prisutan = true;
+
+          if (prijavljeni[br_korisnika-1] == "/")
+          {
+            novi = true;
+            
+            prijavljeni[br_korisnika-1] = line_ip + "|" + mac + "|" + t + "|1";
+          }
+        }
+        stat_info = STAILQ_NEXT(stat_info, next); 
       }
-    } 
+
+      myFile2 = SD.open(date + ".txt", FILE_WRITE);
+      if (myFile2)
+      {
+        if (novi)
+        {
+          Serial.print(br_korisnika);
+          Serial.println(". student is in range.");
+          myFile2.println(prijavljeni[br_korisnika-1]);
+        }
+        else if (!prisutan)
+        {
+          if (prijavljeni[br_korisnika-1] != "/")
+          {
+            int n = prijavljeni[br_korisnika-1].length();
+            prijavljeni[br_korisnika-1][n-1] = '0';
+            
+            Serial.print(br_korisnika);
+            Serial.println(". student is out of range.");
+            myFile2.println(prijavljeni[br_korisnika-1]);
+            prijavljeni[br_korisnika-1] = "/";
+          }
+        }
+        myFile2.close();
+      }         
+      else
+      {
+        Serial.println("error opening " + date + ".txt");
+      }
+      br_korisnika++;
+
+      line_ip = myFile.readStringUntil('\n');
+    }
     myFile.close();
   }
   else
   {
-    Serial.println("error opening ARP_" + datum + ".txt");
+    Serial.println("error opening reg.txt");
   }
-
   delay(500);
 }
 
@@ -282,10 +191,14 @@ String convertDateToStr(const RtcDateTime& dt)
 
     snprintf_P(datestring, 
             countof(datestring),
-            PSTR("%02u%02u%04u"),
+            PSTR("%02u_%02u"),
             dt.Day(),
-            dt.Month(),
-            dt.Year());
+            dt.Month());
     datum = datestring;
     return datestring;
+}
+
+String IpAddress2String(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + "." + String(ipAddress[1]) + String(".") + String(ipAddress[2]) + "." + String(ipAddress[3]);
 }
