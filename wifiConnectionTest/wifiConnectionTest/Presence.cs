dde8fe1c1986487c3fcb21da19f856f143ac8c7d
Sekulovic Milorad <ms251064@ncr.com>
2017-11-17 12:32:33 +0100

    added api reading and parsing c# project

---
+++ wifiConnectionTest/wifiConnectionTest/Presence.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wifiConnectionTest
+{
+    class Presence
+    {
+        public Presence(string ime, string prezime, string id, DateTime ulaz, DateTime izlaz)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            Id = id;
+            Ulaz = ulaz;
+            Izlaz = izlaz;
+        }
+
+        public string Ime { get;}
+        public string Prezime { get;}
+        public DateTime Ulaz { get;}
+        public DateTime Izlaz { get;}
+        public string Id { get;}
+    }
+
+    class PresenceReg
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Id { get; set; }
+    }
+
+    class PresenceTemp
+    {
+        public PresenceTemp() { }
+
+        public PresenceTemp(string raw)
+        {
+            var splitLine = raw.Split('|');
+            this.Ime = splitLine[0];
+            SetVrijeme(splitLine[1]);
+            SetUlazIzlaz(splitLine[2]);
+        }
+
+        public PresenceTemp(string ime, string vrijeme, string ulazIzlaz)
+        {
+            Ime = Ime;
+            SetVrijeme(vrijeme);
+            SetUlazIzlaz(ulazIzlaz);
+        }
+
+        public PresenceTemp(string[] raw)
+        {
+            Ime = raw[0];
+            SetVrijeme(raw[1]);
+            SetUlazIzlaz(raw[2]);
+        }
+
+        public string Ime { get; set; }
+        public DateTime Vrijeme { get; set; }//private setter
+
+        public void SetVrijeme(string value)
+        {
+            DateTime oDate = DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm", null);
+            Vrijeme = oDate;
+        }
+
+        public bool UlazIzlaz { get; set; }//private setter
+
+        public void SetUlazIzlaz(string value)
+        {
+            bool oBool = bool.Parse(value);
+            UlazIzlaz = oBool;
+        }
+    }
+}
---
+++ wifiConnectionTest/wifiConnectionTest/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using ClosedXML.Excel;
+using NativeWifi;
+
+namespace wifiConnectionTest
+{
+    class Program
+    {
+
+        private static string myResponse = String.Empty;
+        private static List<Presence> objectList;
+        static void Main(string[] args)
+        {
+            //firstConnectionTest();
+            //ConnectToWifi();
+            MakeRequest();
+            ParseResult(); // needs work
+            //WriteListToExcel(); // needs work
+            WriteToExcel(); // needs work
+        }
+
+        private static void WriteToExcel()
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                Console.WriteLine("Excel not installed");
+            }
+            object misValue = System.Reflection.Missing.Value;
+            var xlWorkBook = xlApp.Workbooks.Add(misValue);
+           var xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.Item[1];
+            xlWorkSheet.Cells[1, 1] = "ID";
+            xlWorkSheet.Cells[1, 2] = "Name";
+            xlWorkSheet.Cells[1, 3] = "Prezime";
+            xlWorkSheet.Cells[1, 4] = "Ulaz";
+            xlWorkSheet.Cells[1, 5] = "Izlaz";
+            int i = 2;
+            foreach (var obj in objectList)
+            {
+                xlWorkSheet.Cells[i, 1] = obj.Id;
+                xlWorkSheet.Cells[i, 2] = obj.Ime;
+                xlWorkSheet.Cells[i, 3] = obj.Prezime;
+                xlWorkSheet.Cells[i, 4] = obj.Ulaz.ToString("yyyy-MM-ddTHH:mm");
+                xlWorkSheet.Cells[i++, 5] = obj.Izlaz.ToString("yyyy-MM-ddTHH:mm");
+            }
+
+            xlWorkBook.SaveAs(Directory.GetCurrentDirectory()+"\\filename.xls",
+                Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+                Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private static void WriteListToExcel()
+        {
+            //closed excel library
+            var workbook = new XLWorkbook();
+            workbook.AddWorksheet("sheetName");
+            var ws = workbook.Worksheet("sheetName");
+
+            int row = 1;
+            foreach (Presence item in objectList)
+            {
+                ws.Cell("A" + row.ToString()).Value = item.ToString();
+                row++;
+            }
+
+            workbook.SaveAs("filename.xlsx");
+        }
+
+        private static void ParseResult()
+        {
+            List<string> presenceList = myResponse.Replace("\n", "").Split('\r').ToList<string>();
+            objectList = new List<Presence>();
+            foreach (string line in presenceList)
+            {
+                var splitLine = line.Split('|');
+                Presence currPresence = new Presence(splitLine[0],splitLine[1],splitLine[2], DateTime.ParseExact(splitLine[3], "yyyy-MM-ddTHH:mm",null), DateTime.ParseExact(splitLine[4], "yyyy-MM-ddTHH:mm", null));
+                objectList.Add(currPresence);
+            }
+            //foreach (var line in presenceList)
+            //{
+            //    var splitLine = line.Split('|');
+            //    //rucno
+            //    DateTime oDate = DateTime.ParseExact(splitLine[1], "yyyy-MM-ddTHH:mm", null);
+            //    bool oBool = Boolean.Parse(splitLine[2]);
+            //    PresenceTemp presT = new PresenceTemp()
+            //    {
+            //        Ime = splitLine[0],
+            //        UlazIzlaz = oBool,
+            //        Vrijeme = oDate
+            //    };
+            //    //putem metoda
+            //    PresenceTemp presT2 = new PresenceTemp() { Ime = splitLine[0]};
+            //    presT.SetUlazIzlaz(splitLine[2]);
+            //    presT.SetVrijeme(splitLine[1]);
+            //    //putem konstruktora #1
+            //    PresenceTemp presT3 = new PresenceTemp(line);
+            //    //putem konstruktora #2
+            //    PresenceTemp presT4 = new PresenceTemp(splitLine[0],splitLine[1],splitLine[2]);
+            //    //putem konstruktora #3
+            //    PresenceTemp presT5 = new PresenceTemp(splitLine);
+            //    //dodajemo objekat u listu
+                //objectList.Add(presT); // svejedno koji jer su svi isti
+            //}
+            // get reg list
+            //write to excel
+        }
+
+        public static void MakeRequest()
+        {
+            //var url = "http://192.168.4.1/metoda3?polje_pass=wefwe&polje_user=evwv";
+            var url = "https://jsonplaceholder.typicode.com/posts/1";
+            string html = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);//
+            request.Method = "Get";
+            //request.KeepAlive = true;
+            //request.Timeout = 60;
+            ////request.ContentType = "text/plain";
+            ////request.Headers.Add("Content-Type", "text/plain");
+            //request.ContentType = "application/x-www-form-urlencoded";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            myResponse = "";
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                myResponse = sr.ReadToEnd();
+            }
+        }
+
+        public static void ConnectToWifi()
+        {
+            WlanClient client = new WlanClient();
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+                Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllManualHiddenProfiles);
+                foreach (Wlan.WlanAvailableNetwork network in networks)
+                {
+                        var ssid = Encoding.UTF8.GetString(network.dot11Ssid.SSID).ToCharArray().Where(x => x != '\0').Aggregate("", (current, x) => current + x);
+                        Console.WriteLine("Found network with SSID {0}.", ssid);
+                }
+
+                // Retrieves XML configurations of existing profiles.
+                // This can assist you in constructing your own XML configuration
+                // (that is, it will give you an example to follow).
+                var profileName = "";
+                var profileXml = "";
+                foreach (Wlan.WlanProfileInfo profileInfo in wlanIface.GetProfiles())
+                {
+                    string name = profileInfo.profileName; // this is typically the network's SSID
+                    string xml = wlanIface.GetProfileXml(profileInfo.profileName);
+                    if (name == "Proba")
+                    {
+                        profileName = name;
+                        profileXml = xml;
+                    }
+                }
+                wlanIface.SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
+                wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
+
+
+            }
+        }
+    }
+}
