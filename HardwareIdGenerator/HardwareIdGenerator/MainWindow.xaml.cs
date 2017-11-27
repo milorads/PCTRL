f910a8c10248c3205f21d729667f74c1c694e24b
Sekulovic Milorad <ms251064@ncr.com>
2017-11-27 11:43:54 +0100

    qr code generator done

---
+++ HardwareIdGenerator/HardwareIdGenerator/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HardwareIdGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var qrFileName = IdGenerator.IdGenerator.GetId(out var id, out var hashedId);
+            Image finalImage = new Image();
+            BitmapImage logo = new BitmapImage();
+            logo.BeginInit();
+            logo.UriSource = new Uri(qrFileName);
+            logo.EndInit();
+            QrSlika.Source = logo;
+            IdHashed.Content = hashedId;
+        }
+    }
+}
---
+++ HardwareIdGenerator/HashWrapper/QrCodeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+
+namespace IdGenerator
+{
+    public class QrCodeGenerator
+    {
+        internal static string GenerateQrCode(string input, string shortId)
+        {
+            var barcodeWriter = new BarcodeWriter {Format = BarcodeFormat.QR_CODE};
+            barcodeWriter
+                .Write(input)
+                .Save(Directory.GetCurrentDirectory()+$"\\QrCode_{shortId}");
+            return Directory.GetCurrentDirectory() + $"\\QrCode_{shortId}";
+        }
+    }
+}
