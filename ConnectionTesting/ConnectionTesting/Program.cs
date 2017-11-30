2934fa59a0e8355399903a4de03e0f3a0d796841
blukovac <lb130415d@student.etf.bg.ac.rs>
2017-11-30 11:50:49 +0100

    uncommited

--- ConnectionTesting/ConnectionTesting/Program.cs
+++ ConnectionTesting/ConnectionTesting/Program.cs
@@ -13,8 +13,8 @@ namespace ConnectionTesting
 
         static void Main(string[] args)
         {
-            //var url = "http://192.168.4.1/metoda3?polje_pass=wefwe&polje_user=evwv";
-            var url = "https://jsonplaceholder.typicode.com/posts/1";
+            var url = "http://192.168.4.1/metoda3?polje_user=a&polje_password=a";
+            //var url = "https://jsonplaceholder.typicode.com/posts/1";
             string html = string.Empty;
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);//
@@ -31,7 +31,8 @@ namespace ConnectionTesting
             {
                 myResponse = sr.ReadToEnd();
             }
-            List<string> presenceList = myResponse.Replace("\n", "").Split('\r').ToList<string>();
+            var presenceList = myResponse.Replace("\n", "").Split('\r');
+            //var presenceList = pList.tol
             objectList = new List<Presence>();
             foreach (string line in presenceList)
             {
@@ -39,42 +40,6 @@ namespace ConnectionTesting
                 Presence currPresence = new Presence(splitLine[0], splitLine[1], splitLine[2], DateTime.ParseExact(splitLine[3], "yyyy-MM-ddTHH:mm", null), DateTime.ParseExact(splitLine[4], "yyyy-MM-ddTHH:mm", null));
                 objectList.Add(currPresence);
             }
-        }
-        private static void ParseResult()
-        {
-
-            //foreach (var line in presenceList)
-            //{
-            //    var splitLine = line.Split('|');
-            //    //rucno
-            //    DateTime oDate = DateTime.ParseExact(splitLine[1], "yyyy-MM-ddTHH:mm", null);
-            //    bool oBool = Boolean.Parse(splitLine[2]);
-            //    PresenceTemp presT = new PresenceTemp()
-            //    {
-            //        Ime = splitLine[0],
-            //        UlazIzlaz = oBool,
-            //        Vrijeme = oDate
-            //    };
-            //    //putem metoda
-            //    PresenceTemp presT2 = new PresenceTemp() { Ime = splitLine[0]};
-            //    presT.SetUlazIzlaz(splitLine[2]);
-            //    presT.SetVrijeme(splitLine[1]);
-            //    //putem konstruktora #1
-            //    PresenceTemp presT3 = new PresenceTemp(line);
-            //    //putem konstruktora #2
-            //    PresenceTemp presT4 = new PresenceTemp(splitLine[0],splitLine[1],splitLine[2]);
-            //    //putem konstruktora #3
-            //    PresenceTemp presT5 = new PresenceTemp(splitLine);
-            //    //dodajemo objekat u listu
-            //objectList.Add(presT); // svejedno koji jer su svi isti
-            //}
-            // get reg list
-            //write to excel
-        }
-
-        public static void MakeRequest()
-        {
-
         }
     }
 }
