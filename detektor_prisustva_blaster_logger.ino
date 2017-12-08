4b90efc07123a7657c90d19f9d7792e47cd7338f
blukovac <lb130415d@student.etf.bg.ac.rs>
2017-12-08 14:53:44 +0100

    ubacen hostname

--- detektor_prisustva_blaster_logger.ino
+++ detektor_prisustva_blaster_logger.ino
@@ -6,7 +6,7 @@
 #include <ESP8266WebServer.h>
 #include <SPI.h>
 #include "MD5.h"
-//#include <DNSServer.h>
+#include <DNSServer.h>
 
 extern "C" 
 {
@@ -14,14 +14,14 @@ extern "C"
 }
 
 #define countof(a) (sizeof(a)/sizeof(a[0]))
-#define MAX 100
+#define MAX 5
 
 const char *ssid     = "ProbaBoban";
 const char *webPage  = "<!DOCTYPE HTML> <head><title>Registracija</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/regData\"><h1>Registracija</h1><table><tr><td>Ime:</td><td><input type=\"text\" name=\"polje_ime\" required = \"required\"></td></tr> <tr><td>Prezime:</td><td><input type=\"text\" name=\"polje_prezime\" required = \"required\"></td></tr> <tr><td>Id:</td><td><input type=\"text\" name=\"polje_id\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Submit</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
-const char *webPage2 = "<!DOCTYPE HTML> <head><title>Admin</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/getData\"><h1>Hello admin!</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr> <tr><td>File name:</td><td><input type=\"text\" name=\"file\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
-const char *webPage3 = "<!DOCTYPE HTML> <head><title>Brisanje</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/deleteData\"><h1>Brisanje fajla</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr><tr><td>Datum fajla:</td><td><input type=\"text\" name=\"file\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
-const char *webPage4 = "<!DOCTYPE HTML> <head><title>Test</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/apiTest\"><h1>Test</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
-const char *webPage5 = "<!DOCTYPE HTML> <head><title>List</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/listData\"><h1>List of files</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
+//const char *webPage2 = "<!DOCTYPE HTML> <head><title>Admin</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/getData\"><h1>Hello admin!</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr> <tr><td>File name:</td><td><input type=\"text\" name=\"file\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
+//const char *webPage3 = "<!DOCTYPE HTML> <head><title>Brisanje</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/deleteData\"><h1>Brisanje fajla</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr><tr><td>Datum fajla:</td><td><input type=\"text\" name=\"file\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
+//const char *webPage4 = "<!DOCTYPE HTML> <head><title>Test</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/apiTest\"><h1>Test</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
+//const char *webPage5 = "<!DOCTYPE HTML> <head><title>List</title> </head><html> <form name=\"form1\" id=\"txt_form\" method=\"get\" action=\"/listData\"><h1>List of files</h1><table><tr><td>Code:</td><td><input type=\"text\" name=\"code\" required = \"required\"></td></tr> <tr><td><button type=\"submit\">Continue</button></td><td><button type=\"reset\">Reset</button></td></tr>  </form></html>";
 const char *err_msg1 = "<!DOCTYPE HTML> <head><title>Error</title> </head><html> <font color=\"red\"> Greska pri unosu! </font></html>";
 const char *err_msg2 = "<!DOCTYPE HTML> <head><title>Success</title> </head><html> <font color=\"green\"> Uspesan unos! </font></html>";
 const char *err_msg3 = "<!DOCTYPE HTML> <head><title>Warning</title> </head><html> <font color=\"green\"> Vec si registrovan! </font></html>";
@@ -38,22 +38,24 @@ DS1302 rtc(0, 4, 5);
 struct station_info *stat_info;
 struct ip_addr *IPaddress;
 IPAddress address;
-struct registrovani
+struct Registrovani
 {
   uint8_t mac[6];
   String ime;
   String prezime;
   String id;
   String vreme_ulaska = "";
+  uint8_t size_reg = MAX;
 };
-registrovani reg[MAX];
+//registrovani reg[MAX];
+Registrovani* reg = new Registrovani[MAX];
 int reg_num = 0;
 String salt = "PCTRL";
 String md5 = "164803c7f69b529f116352cc0814084e";
 //dns server
-/* const byte DNS_PORT = 53;
+const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 4, 1);
-DNSServer dnsServer; */
+DNSServer dnsServer;
 
 void handleRoot()
 {
@@ -101,7 +103,7 @@ void handleRoot()
   }
 }
 
-void apiPage()
+/* void apiPage()
 {
   server.send(200, "text/html", webPage4);
 }
@@ -119,7 +121,7 @@ void deletePage()
 void listPage()
 {
   server.send(200, "text/html", webPage5);
-}
+} */
 
 void handleData()
 {
@@ -200,6 +202,18 @@ void handleData()
       }
       logFile = SD.open("loging.txt",FILE_WRITE);
       reg_num++;
+      if (reg_num == reg->size_reg)
+      {
+        Registrovani* reg_pom = reg;
+        reg = new Registrovani[reg->size_reg+MAX];
+        for (int i = 0; i < reg_num; i++)
+        {
+          reg[i] = reg_pom[i];
+        }
+        delete[](reg_pom);
+        reg->size_reg = reg->size_reg+MAX;
+        Serial.println(reg->size_reg);
+      }
       logFile.print(reg_num);
       logFile.println(". registrovani korisnik: " + reg_kor);
       server.send(200, "text/html", err_msg2);
@@ -280,7 +294,7 @@ void deleteData()
       datum[i++] = 't';
       datum[i++] = 'x';
       datum[i++] = 't';
-      datum[i] = '\0';
+      datum[i]   = '\0';
       if (SD.remove(datum))
       {
         server.send(200, "text/html", succ_remove);
@@ -385,35 +399,37 @@ void setup(void)
   Serial.begin(115200);
   Serial.println();
   Serial.print("Configuring access point...");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(ssid);
   Serial.println("");
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
-  //dns config
-  /* dnsServer.setTTL(300);
-  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
-  dnsServer.start(DNS_PORT, "boban.coban", apIP); 
-  server.onNotFound([](){
-                          String message = "Hello World!\n\n";
-                          message += "URI: ";
-                          message += server.uri();
-                          server.send(200, "text/plain", message);
-                        }
-  ); *///kraj dns-a
   struct softap_config config;
   wifi_softap_get_config(&config); // Get config first.
   config.max_connection = 30; // how many stations can connect to ESP8266 softAP at most.
   wifi_softap_set_config(&config);// Set ESP8266 softap config
   server.on("/", handleRoot);
   server.on("/regData", handleData);
-	server.on("/file", adminPage);
+  //server.on("/file", adminPage);
   server.on("/getData", getData);
-  server.on("/delete", deletePage);
+  //server.on("/delete", deletePage);
   server.on("/deleteData", deleteData);
-  server.on("/api", apiPage);
+  //server.on("/api", apiPage);
   server.on("/apiTest", apiTest);
-  server.on("/list", listPage);
+  //server.on("/list", listPage);
   server.on("/listData", listData);
+  //dns config
+  dnsServer.setTTL(300);
+  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+  dnsServer.start(DNS_PORT, "prijavi.me", apIP); 
+  server.onNotFound([](){
+                          String message = "Hello World!\n\n";
+                          message += "URI: ";
+                          message += server.uri();
+                          server.send(200, "text/plain", message);
+                        }
+  ); //kraj dns-a
   server.begin();
   Serial.println("HTTP server started");
   //SD Card Initialization
@@ -465,6 +481,18 @@ void setup(void)
         reg[reg_num].id.replace("\n", "");
         reg[reg_num].id.replace("\r", "");
         reg_num++;
+        if (reg_num == reg->size_reg)
+        {
+          Registrovani* reg_pom = reg;
+          reg = new Registrovani[reg->size_reg+MAX];
+          for (int i = 0; i < reg_num; i++)
+          {
+            reg[i] = reg_pom[i];
+          }
+          delete[](reg_pom);
+          reg->size_reg = reg->size_reg+MAX;
+          Serial.println(reg->size_reg);
+        }
       }
       myFile.close();
     }
@@ -491,14 +519,14 @@ void setup(void)
 void loop(void)
 {
   //dns processing
-  //dnsServer.processNextRequest();
+  dnsServer.processNextRequest();
   server.handleClient();
   client_status();
 }
 
 void client_status()
 {
-  for (int i = 0; i <= reg_num; i++)
+  for (int i = 0; i < reg_num; i++)
   {
     bool prisutan = false;
     String t = convertToStr(rtc.getTime()); 
